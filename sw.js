cf48b6ca00d0937f59d8cbdf5ea033e458421f82
ohbang123-cmyk <ohbang123@gmail.com>
2026-09-23 08:35:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,11 @@
-const CACHE='mync-civil-20260922-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
+const CACHE='minbeop-touch-v2';
+const ASSETS=['./','./index.html','./questions.json','./answers.json','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{
-  if(e.request.url.startsWith('https://api.openai.com/')) return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  if (e.request.url.endsWith('/answers.json') || e.request.url.endsWith('/questions.json')) {
+    e.respondWith(fetch(e.request).then(r=>{ const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r; }).catch(()=>caches.match(e.request)));
+  } else {
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  }
 });
